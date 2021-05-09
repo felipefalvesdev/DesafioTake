6f79dabe15f0c7c94b26dcdd25c554834d27f183
felip <felip@DESKTOP-B58D9BS>
2021-05-09 14:42:34 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioTake/Controllers/RepositoryController.cs
@@ -0,0 +1,35 @@
+﻿using DesafioTake.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DesafioTake.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepositoryController : ControllerBase
+    {
+        [HttpGet]
+        public  async Task<string> GetRepositoryByLanguageAndCreatedDate(string language, int NumberOfResults)
+        {
+            List<Repository.Details> result = await API.URLConnection();
+            List<Repository.Details> RepositoriesByLanguage = new List<Repository.Details>();
+            int count = 0;
+            
+            foreach(Repository.Details obj in result)
+            {
+                if(obj.Language == language)
+                {
+                    RepositoriesByLanguage.Add(obj);
+                    count += 1;
+                    if (count == NumberOfResults) break;
+                }
+            }
+            var serialized = JsonConvert.SerializeObject(RepositoriesByLanguage);
+            return serialized;
+        }         
+
+    }
+}
---
+++ DesafioTake/Services/API.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DesafioTake.Services
+{
+    public class API
+    {
+        /// <summary>
+        /// Este metodo é responsavel por conectar a api do github e traz todos os repositorios de determinada org.
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<List<Repository.Details>> URLConnection()
+        {
+            try
+            {
+                
+                HttpClient httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri("https://api.github.com/");
+
+                httpClient.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("DesafioTake", "1.0"));
+                httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await httpClient.GetAsync("orgs/takenet/repos?per_page=100");
+
+                string RepositoryJsonString = await response.Content.ReadAsStringAsync();
+
+                List<Repository.Details> deserialized = JsonConvert.DeserializeObject<List<Repository.Details>>(RepositoryJsonString);
+
+                List<Repository.Details> repositoriesByCreatedDateDesc = deserialized.OrderBy(x => x.CreatedAt).ToList();
+
+
+
+                return repositoriesByCreatedDateDesc;
+
+                //IEnumerable<Repository.Details> deserialized = JsonConvert.DeserializeObject<IEnumerable<Repository.Details>>(RepositoryJsonString);
+
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
